84be8b5f479a2a463d288a50e3847b80e89f625d
arifremal <arifremal013@gmail.com>
2023-03-09 14:49:06 +0600

    updated

--- js/utilities.js
+++ js/utilities.js
@@ -11,9 +11,10 @@ function getTextElementValueById(elementId) {
   const textElementValueString = textElement.innerText;
   const textElementValue = parseFloat(textElementValueString);
   return textElementValue;
+  
 }
 
 function setTextElementValueById(elementId, newValue) {
   const textElement = document.getElementById(elementId);
-  textElement.innerText = newValue;
+  textElement.innerText= newValue;
 }
